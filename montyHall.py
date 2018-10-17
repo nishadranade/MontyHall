9018530eab5c46cd8a5913a29705894f0bc101b3
Nishad Ranade <34412571+nishadranade@users.noreply.github.com>
2018-10-17 08:57:24 -0400

    Update montyHall.py

--- montyHall.py
+++ montyHall.py
@@ -8,24 +8,15 @@ pd.set_option('display.width', 500)
 pd.set_option('display.max_columns', 100)
 pd.set_option('display.notebook_repr_html', True)
 
-
-#x = np.random.randint(0, 3, 3, int)
-#print(x)
-
-
-
+#simulate the position of the prize
 def simulate_host(n):
     return np.random.randint(0, 3, n, int)
 
+#simulate the player's first guess
 def simulate_guess(n):
     return np.zeros(n)
 
-#x = simulate_host(nsim)
-#y = simulate_guess(nsim)
-
-#second_host = np.zeros(nsim)
-
-
+#simulates the empty door the host opens 
 def open_door(n, x, second_host):
     i = 0
     while i < n:
@@ -38,7 +29,7 @@ def open_door(n, x, second_host):
         i = i+1
     return
 
-
+# simulates the player's second guess
 def second_guess(n, y, second_host):
     i = 0
     while i < n:
@@ -48,9 +39,7 @@ def second_guess(n, y, second_host):
         i = i+1
     return
 
-
-
-
+#checks how many times the player was right
 def correct(n, x, y):
     count = 0
     i = 0
@@ -60,32 +49,17 @@ def correct(n, x, y):
         i = i+1
     return count
 
-#def plot_result(numCorrect):
-
-
 #execute the functions and store the obtained values in appropriate variables
-
 simulations = 10000;
 
 x1 = simulate_host(simulations)
 x2 = simulate_guess(simulations)
-
-
 second_chance = np.zeros(simulations)
-
-
 open_door(simulations, x1, second_chance)
-
-
-
 second_guess(simulations, x2, second_chance)
-
-
 result = correct(simulations, x1, x2)
 
-
-print(result)
-
+# plot a pie diagram using matplotlib
 labels = 'Correct Guess', 'Wrong Guess'
 sizes = [ result, simulations - result]
 colors = [ 'green', 'red']
